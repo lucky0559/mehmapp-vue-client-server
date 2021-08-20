a5883d99b02105731797d1eba393116d65594030
Lucky Angelo Rabosa <angelorabosa55@yahoo.com.ph>
2021-08-20 15:05:34 +0800

    added pool

--- server/src/db/database.js
+++ server/src/db/database.js
@@ -1,5 +1,19 @@
 const mysql = require('mysql2');
 
-module.exports = mysql.createConnection({
-   uri: 'mysql://u3nhojkyfzisauwr:nhtMziGpaFlvqnK9yV7s@b2dql3cn1qsogappkey0-mysql.services.clever-cloud.com:3306/b2dql3cn1qsogappkey0'
+const uri = 'mysql://u3nhojkyfzisauwr:nhtMziGpaFlvqnK9yV7s@b2dql3cn1qsogappkey0-mysql.services.clever-cloud.com:3306/b2dql3cn1qsogappkey0'
+
+ const connection = mysql.createConnection({
+   uri: uri
 })
+
+let pool = mysql.createPool(uri);
+
+pool.on('connection', function (_conn) {
+    if (_conn) {
+        logger.info('Connected the database via threadId %d!!', _conn.threadId);
+        _conn.query('SET SESSION auto_increment_increment=1');
+    }
+});
+
+
+module.exports = connection
